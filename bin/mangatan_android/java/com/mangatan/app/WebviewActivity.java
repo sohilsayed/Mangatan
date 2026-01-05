2732d68878f9c9d81b8fd659686f30c9c873cc50
sohilsayed <66560257+sohilsayed@users.noreply.github.com>
2026-01-05 05:34:27 +0200

    android: Make the webview fullscreen by default

--- bin/mangatan_android/java/com/mangatan/app/WebviewActivity.java
+++ bin/mangatan_android/java/com/mangatan/app/WebviewActivity.java
@@ -2,6 +2,7 @@ package com.mangatan.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;          
 import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -20,7 +21,16 @@ public class WebviewActivity extends Activity {
         // Must be called before setContentView
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         // -----------------------------
-
+        
+        // Fullscreen immersive mode 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
         myWebView = new WebView(this);
         setContentView(myWebView);
 
